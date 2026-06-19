e8399f29cb9830eca6be9aa142bfaadf2395b713
ajklawdda <uceba259@gmail.com>
2026-06-20 00:51:44 +0300

    Update gmail_reader.py

--- gmail_reader.py
+++ gmail_reader.py
@@ -481,5 +481,5 @@ if __name__ == "__main__":
     time.sleep(5)
 
     # Запускаем веб-сервер (Render ожидает, что сервис слушает порт)
-    # port = int(os.environ.get('PORT', 10000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
